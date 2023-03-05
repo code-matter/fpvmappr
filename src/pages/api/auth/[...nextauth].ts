76c725918a4ae63966ce6f38322ebd035ae0dd29
Alex Caissy <acaissy@planifika.net>
2023-03-04 21:00:51 -0500

    [FEATURE] - added github and discord providers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
@@ -7,6 +9,14 @@ export default NextAuth({
       // TODO: Add the dev | prod differenciation both here AND on google console!
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? ""
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? ""
     })
   ],
   secret: process.env.JWT_SECRET
